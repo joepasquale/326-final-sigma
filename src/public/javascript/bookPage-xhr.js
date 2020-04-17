6ea81bcc089913243cd11330a53fdd255fed9fa3
DanSC111 <danielshanecoley@gmail.com>
2020-04-17 17:03:30 -0400

    changed file names edited some HTML with missing stuff

---
+++ src/public/javascript/bookPage-xhr.js
@@ -0,0 +1,45 @@
+const url = "http://localhost:4000";
+
+async function addReview() {
+
+    let Review = document.getElementById("reviewText").value;
+
+    const data = {"Review" : Review};
+        const newURL = "http://localhost:4000/api/book/review";  //Check later **************************
+    await fetch(newURL, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    })
+        .then(function (dat) { 
+        console.log('Request success: ', dat.results);
+    })
+        .catch(function (error) {
+            console.log('Request failure: ', error);
+        });
+}
+
+async function addToList() {
+
+    let Review = document.getElementById("reviewText").value;
+
+    const data = { "List" : List};
+        const newURL = "http://localhost:4000/api/book/addList";  //Check later***************************
+    await fetch(newURL, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    })
+        .then(function (dat) { 
+        console.log('Request success: ', dat.results);
+    })
+        .catch(function (error) {
+            console.log('Request failure: ', error);
+        });
+}
