2168b0947c51bae9baf5595fb748a41f848b0969
DanSC111 <danielshanecoley@gmail.com>
2020-04-20 14:32:29 -0400

    Slight signup.html fix and added password length/match checking
    signup.html was using the login function rather than the signup function and also had the older signup still in, so i commented it out.
    
    I also edited the signup function so passwords are of the correct length and match by looking back at the regex mini project to output to HTML.

--- src/public/javascript/signup-xhr.js
+++ src/public/javascript/signup-xhr.js
@@ -5,8 +5,16 @@ async function registerUser() {
     let username = document.getElementById("inputUsername").value;
     let passwordA = document.getElementById("inputPassword").value;
     let passwordB = document.getElementById("confirmPassword").value;
-    if(passwordA !== passwordB){ //Check if passwords match ******COME BACK TO THIS LATER******** -Dan
 
+    if((passWordA.length <= 5 )||( passWordA.length >= 21)){ //Check if password is the correct length (6-20), responds and stops if not
+        let returnTextLength = "Password not of appropriate length (6-20 characters)";
+        returnTextLength.innerHTML = passwordHelpBlock;
+        return;
+    }
+    else if(passwordA != passwordB){ //Check if passwords match, responds and stops if not
+        let returnTextMatch = "Password not of appropriate length (6-20 characters)";
+        returnTextMatch.innerHTML = passwordHelpBlock;
+        return;
     }
     const data = { "email" : email, "username": username, "password": passwordA };
         const newURL = url+"/api/login/register";
