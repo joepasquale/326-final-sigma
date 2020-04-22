bf6feaee21a0af958fa9f8f3189dae279bc18304
DanSC111 <danielshanecoley@gmail.com>
2020-04-22 18:11:00 -0400

    continued work on homefeed-xhr

--- src/public/javascript/homefeed-xhr.js
+++ src/public/javascript/homefeed-xhr.js
@@ -22,8 +22,21 @@ async function submitComment() {
         });
 }
 
+async function submitComment() {
 
-async function movePage(){ //moves from the homefeed page to homefeedAdd page
+    let Comment = document.getElementById("commentText").value;
+    let UserName = document.getElementById("username").value; //revist once users are added********************
 
+    const data = {"Comment" : Comment, "UserName" : UserName};
+    const newURL = "http://localhost:4000/api/book/review";  //Check later **************************
+    const resp = await postData(newURL, data);
+}
 
+async function handleBook(bookData) {
+    console.log(bookData);
+    document.title = 'Shelf - ' + bookData.title;
+    document.getElementById('title').innerHTML = bookData.title;
+    document.getElementById('image_cover').src = bookData.imageLinks.thumbnail;
+    document.getElementbyId('reviewerName').innerHTML = bookData.userReview[0].username // 
+    document.getElementbyId('reviewerText').innerHTML = bookData.userReview[0].reviewText //check with josh
 }
