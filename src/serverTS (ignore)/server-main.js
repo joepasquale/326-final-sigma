9c015aa84c777a1139159b949111fe3331508f39
Josh Vasilevsky <josh.vasilevsky@gmail.com>
2020-04-22 14:24:39 -0400

    removed un-needed files

--- src/serverTS (ignore)/server-main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-var Database_1 = require("./Database");
-var myserver_routing_1 = require("./myserver-routing");
-var theDatabase = new Database_1.Database('mongodb://localhost:27017/shelf');
-var theServer = new myserver_routing_1.MyServer(theDatabase);
-theServer.listen(4000);
-//# sourceMappingURL=server-main.js.map
--- src/serverTS (ignore)/server-main.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import { Database } from './Database';
-import { MyServer } from './myserver-routing';
-
-const theDatabase = new Database('mongodb://localhost:27017/shelf');
-const theServer = new MyServer(theDatabase);
-
-theServer.listen(4000);
