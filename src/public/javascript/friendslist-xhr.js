0653ff2d7f55ac577e433db4a60b56db47f641db
Joseph Pasquale <32305551+joepasquale@users.noreply.github.com>
2020-04-24 19:07:49 -0400

    added some functionality to friends list page

--- src/public/javascript/friendslist-xhr.js
+++ src/public/javascript/friendslist-xhr.js
@@ -1,11 +1,48 @@
-async function addFriend(){
-    let x = 0;
+let userProfile;
+
+async function readProfile(id) {
+    let newURL = url + '/api/user/read';
+    const data = { "id": id };
+    const resp = await postData(newURL, data);
+    if (resp.status != 200) {
+        console.log("Profile Doesn't Exist");
+        window.location.href = url + '/auth/search-results.html?q=';
+        return;
+    }
+    let j = await resp.json();
+    return j;
 }
 
-async function removeFriend(){
-    let x = 0;
+async function getProfile() {
+    let urlData = await parseURL();
+    let userID = urlData.user;
+    let decoded = decodeURIComponent(userID);
+    let j = await readProfile(decoded);
+    console.log(j);
+    userProfile = j;
+    await handleProfile(j);
 }
 
-async function getFriendList(){
-    let x = 0;
+async function acceptFriend() {
+    let userB = await readProfile(currentUser._id);
+    const data = {
+        "UserA": userProfile,
+        "UserB": userB,
+    };
+    const newURL = url + '/api/friends/accept';
+    const resp = await postData(newURL, data);
+}
+
+async function denyFriend() {
+    let userB = await readProfile(currentUser._id);
+    const data = {
+        "UserA": userProfile,
+        "UserB": userB,
+    };
+    const newURL = url + '/api/friends/reject';
+    const resp = await postData(newURL, data);
+}
+
+async function removeFriend() {
+
 }
