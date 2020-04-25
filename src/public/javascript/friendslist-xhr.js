1d1b7b67cd1cba6e576d759185711c7eb225b118
Joseph Pasquale <32305551+joepasquale@users.noreply.github.com>
2020-04-24 21:49:41 -0400

    Update friendslist-xhr.js

--- src/public/javascript/friendslist-xhr.js
+++ src/public/javascript/friendslist-xhr.js
@@ -29,11 +29,25 @@ async function acceptFriend() {
         "UserA": userProfile,
         "UserB": userB,
     };
+    let list = document.getElementById('friends' + id);
+    let listitem = list.createElement('tr');
+    listItem.innerHTML = `
+    <td class="align-middle">
+        <div style="width:45px;">
+            <img src="../resources/avatar.png" alt="..." class="rounded-circle" style="width:50px; height:50px;" }>
+        </div>
+    </td>
+    <td class="align-middle"><a href=">${userB.info.firstname} ${userB.info.lastname}</a></td>
+    <td class="align-middle">${userB.info.email}/td>
+        <td class="align-middle">
+        </td>
+    </tr>`;
     const newURL = url + '/api/friends/accept';
     const resp = await postData(newURL, data);
 }
 
 async function denyFriend() {
+
     let userB = await readProfile(currentUser._id);
     const data = {
         "UserA": userProfile,
@@ -41,6 +55,7 @@ async function denyFriend() {
     };
     const newURL = url + '/api/friends/reject';
     const resp = await postData(newURL, data);
+
 }
 
 async function removeFriend() {
