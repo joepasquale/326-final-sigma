7cf7584415603ad5fd5771352aa23fc7f7f7fdcf
joshvasilevsky <=>
2020-05-01 16:35:27 -0400

    upated dependancies

--- src/server/routes/login.js
+++ src/server/routes/login.js
@@ -36,7 +36,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 exports.__esModule = true;
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcryptjs');
 var router = require("express").Router();
 exports.router = router;
 var user_1 = require("../models/user");
--- src/server/routes/login.ts
+++ src/server/routes/login.ts
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const router = require("express").Router();
 import { User } from '../models/user';
 
