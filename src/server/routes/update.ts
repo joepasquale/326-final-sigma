175ecb2bfdb6fe72f93338118889960a1353d86d
DanSC111 <danielshanecoley@gmail.com>
2020-05-07 12:46:24 -0400

    update tweak

--- src/server/routes/update.ts
+++ src/server/routes/update.ts
@@ -4,10 +4,15 @@ import { Update } from '../models/update';
 
 
 router.post('/all', async (req, res) => {
-    
-    const friendslist = await Friend.find(
+
+    let friendslist = await Friend.find(
         {'requester': req.body.User, 'status': 3},
     );
+    if(!friendslist){
+        friendslist = await Friend.find(
+            {'receiver': req.body.User, 'status': 3},
+        );
+    }
     let friendid = [];
     friendslist.forEach(async (element) => {friendid.push(element.receiver)});
     let updates = await Update.find({
